12f1f1ec8b30eaa5cc1beec9fee37612ee143f59
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-10-17 18:22:26 -0300

    style(backend): lint

--- backend/tests/Feature/UploadAvatarTest.php
+++ backend/tests/Feature/UploadAvatarTest.php
@@ -2,21 +2,21 @@
 
 use Illuminate\Http\UploadedFile;
 
-afterAll( function () {
-    if ( is_dir( storage_path( 'app/testing' ) ) ) {
-        $files = glob( storage_path( 'app/testing' ) . "/*" );
-        foreach ( $files as $file ) {
-            if ( is_file( $file ) ) {
-                unlink( $file );
+afterAll(function () {
+    if (is_dir(storage_path('app/testing'))) {
+        $files = glob(storage_path('app/testing').'/*');
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                unlink($file);
             }
         }
     }
-} );
+});
 
-it( 'should be able to upload an user avatar', function () {
-    $file = UploadedFile::fake()->image( 'avatar.jpg', 800, 600 )->size( 2000 );
+it('should be able to upload an user avatar', function () {
+    $file = UploadedFile::fake()->image('avatar.jpg', 800, 600)->size(2000);
 
-    $response = $this->postJson( '/api/v1/upload', [ 'file' => $file ] );
+    $response = $this->postJson('/api/v1/upload', ['file' => $file]);
 
-    $response->assertStatus( 201 );
-} );
+    $response->assertStatus(201);
+});
